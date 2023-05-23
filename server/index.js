1f87a03b2181aa1abbf76fc7f0ec4fea87f8a065
eliuttth-dev <eliuttth@gmail.com>
2023-05-23 02:46:52 -0300

    Routes And Middleware added

--- server/index.js
+++ server/index.js
@@ -2,10 +2,27 @@ const express = require("express");
 const app = express();
 const config = require("./src/config");
 const cors = require("cors");
+const path = require("path");
+const userRoutes = require("./src/app/routes/userRoutes");
 
 // Middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+
+// Routes
+app.use("/user", userRoutes);
+
+app.get("/home", (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the homepage!");
+});
+app.get("/res", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 // Server
 app.listen(config.port, () => {
